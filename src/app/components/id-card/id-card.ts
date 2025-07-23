cb803dc68b0893550849a8ea9ffb3f46651a4572
Jamez <adobeclip2003@gmail.com>
2025-07-23 21:20:16 +0100

    continuation of id card implementation

--- src/app/components/id-card/id-card.ts
+++ src/app/components/id-card/id-card.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { DashboardService } from '../../services/dashboard/dashboard-service';
 
 @Component({
   selector: 'app-id-card',
@@ -7,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrl: './id-card.css'
 })
 export class IdCard {
+    private dashboardService = inject(DashboardService);
 
+    get genderInitial() {
+        if(this.dashboardService.profile_data() && this.dashboardService.profile_data()?.gender === 'male') return 'm'
+        else if(this.dashboardService.profile_data() && this.dashboardService.profile_data()?.gender === 'female') return 'f'
+        else return 'a'
+    }
 }
