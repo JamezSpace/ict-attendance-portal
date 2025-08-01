2d54fe01298bf48571a0795261ae501ce6ebb975
Jamez <130938946+JamezSpace@users.noreply.github.com>
2025-08-01 22:15:45 +0100

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -33,7 +33,7 @@ export const routes: Routes = [
     {
         path: 'dashboard',
         component: UserDashboard,
-        // canActivate: [authGuard],
+        canActivate: [authGuard],
         children: [
             {
                 path: '',
