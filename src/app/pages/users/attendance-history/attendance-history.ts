492f6ebaf5fefd53d5b2ef189cb291bb4bb54ae2
Jamez <adobeclip2003@gmail.com>
2025-08-03 00:18:22 +0100

    corrected deployment issues

--- src/app/pages/users/attendance-history/attendance-history.ts
+++ src/app/pages/users/attendance-history/attendance-history.ts
@@ -8,13 +8,30 @@ import { DashboardService } from '../../../services/users/dashboard/dashboard-se
   templateUrl: './attendance-history.html',
   styleUrl: './attendance-history.css'
 })
-export class AttendanceHistory implements OnInit{
+export class AttendanceHistory implements OnInit {
   private dashboardService = inject(DashboardService);
   statuses = ['late', 'on time', 'absent']
 
   attendances = this.dashboardService.attendances
 
-  async ngOnInit () {
+  async ngOnInit() {
     await this.dashboardService.getAttendance();
   }
+
+  duration(timeIn: any, timeOut: any): string {
+    const diffMs = new Date(timeOut).getTime() - new Date(timeIn).getTime();
+
+    if (isNaN(diffMs) || diffMs < 0) {
+      return "Invalid or negative time range";
+    }
+
+    const totalSeconds = Math.floor(diffMs / 1000);
+    const days = Math.floor(totalSeconds / (60 * 60 * 24));
+    const hours = Math.floor((totalSeconds % (60 * 60 * 24)) / 3600);
+    const minutes = Math.floor((totalSeconds % 3600) / 60);
+    const seconds = totalSeconds % 60;
+
+    return `${days > 0 ? days + "d " : ""}${hours}h ${minutes}m ${seconds}s`;
+  }
+  
 }
