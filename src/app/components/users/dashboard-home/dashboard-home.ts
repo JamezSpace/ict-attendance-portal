d32cf52dbdc3b4d8037a669e12882928d34949df
Jamez <adobeclip2003@gmail.com>
2025-08-03 18:27:00 +0100

    immdeiate fix needed

--- src/app/components/users/dashboard-home/dashboard-home.ts
+++ src/app/components/users/dashboard-home/dashboard-home.ts
@@ -7,87 +7,105 @@ import { UpdateProfileDialog } from '../../dialogs/update-profile-dialog/update-
 import { Dashboard } from '../../../pages/users/dashboard/dashboard';
 
 @Component({
-  selector: 'app-dashboard-home',
-  imports: [TasksOverview],
-  templateUrl: './dashboard-home.html',
-  styleUrl: './dashboard-home.css'
+    selector: 'app-dashboard-home',
+    imports: [TasksOverview],
+    templateUrl: './dashboard-home.html',
+    styleUrl: './dashboard-home.css'
 })
 export class DashboardHome {
-  private dashboardService = inject(DashboardService)
-  readonly dialog = inject(MatDialog);
-  private router = inject(Router);
+    private dashboardService = inject(DashboardService)
+    readonly dialog = inject(MatDialog);
+    private router = inject(Router);
 
-  userData = Dashboard.userLoggedIn;
-  previous_attendances = this.dashboardService.attendances()
-  user_profile = this.dashboardService.profile_data
+    userData = Dashboard.userLoggedIn;
+    previous_attendances = this.dashboardService.attendances()
+    user_profile = this.dashboardService.profile_data
 
-  get todayDay() {
-    return this.previous_attendances[this.previous_attendances.length - 1].day + 1
-  }
+    get todayDay() {
+        return this.previous_attendances[this.previous_attendances.length - 1].day + 1
+    }
 
-  get currentDate() {
-    return new Date().toLocaleDateString('en-NG', {
-      weekday: 'long',
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric'
-    });
-  }
+    get currentDate() {
+        return new Date().toLocaleDateString('en-NG', {
+            weekday: 'long',
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric'
+        });
+    }
 
-  //   currentTime: WritableSignal<string>;
+    referenceDate ="2025-08-03";
 
-  //   private intervalId: any;
+    get dayOfConvention(): number {
+        const today = new Date();
+        const inputDate = new Date(this.referenceDate);
 
-  //   constructor() {
-  //     this.currentTime = signal(new Date().toLocaleTimeString());
+        // Normalize both dates to midnight (ignore time part)
+        today.setHours(0, 0, 0, 0);
+        inputDate.setHours(0, 0, 0, 0);
 
-  //     this.intervalId = setInterval(() => {
-  //       this.currentTime.set(new Date().toLocaleTimeString());
-  //     }, 1000);
+        const diffInMs = inputDate.getTime() - today.getTime();
+        const diffInDays = Math.floor(diffInMs / (1000 * 60 * 60 * 24));
 
-  //     this.dashboardService.getAttendance()
+        return diffInDays + 1;
+    }
 
-  //   }
 
-  //   ngOnDestroy(): void {
-  //     clearInterval(this.intervalId); // cleanup to avoid memory leak
-  //   }
 
-  signedIn = signal(true);
-  signInStatusText = computed(() => {
+//   currentTime: WritableSignal<string>;
+
+//   private intervalId: any;
+
+//   constructor() {
+//     this.currentTime = signal(new Date().toLocaleTimeString());
+
+//     this.intervalId = setInterval(() => {
+//       this.currentTime.set(new Date().toLocaleTimeString());
+//     }, 1000);
+
+//     this.dashboardService.getAttendance()
+
+//   }
+
+//   ngOnDestroy(): void {
+//     clearInterval(this.intervalId); // cleanup to avoid memory leak
+//   }
+
+signedIn = signal(true);
+signInStatusText = computed(() => {
     return this.signedIn() ? 'You clocked in today at 8:12AM' : "You haven't clocked in yet"
-  })
-  signInActionText = computed(() => {
+})
+signInActionText = computed(() => {
     return this.signedIn() ? 'clock out' : 'clock in'
-  })
-  // officeTiming = {
-  //   start_time: new Date().toLocaleTimeString('en-NG', {
-  //     hour: 'numeric',
-  //     minute: '2-digit',
-  //     hour12: true
-  //   }),
-  //   end_time: new Date().toLocaleTimeString('en-NG', {
-  //     hour: 'numeric',
-  //     minute: '2-digit',
-  //     hour12: true
-  //   })
-  // }
+})
+// officeTiming = {
+//   start_time: new Date().toLocaleTimeString('en-NG', {
+//     hour: 'numeric',
+//     minute: '2-digit',
+//     hour12: true
+//   }),
+//   end_time: new Date().toLocaleTimeString('en-NG', {
+//     hour: 'numeric',
+//     minute: '2-digit',
+//     hour12: true
+//   })
+// }
 
-  navigateToAttendance() {
+navigateToAttendance() {
     this.router.navigate(['dashboard/attendance']);
-  }
+}
 
-  openUpdateProfileData() {
+openUpdateProfileData() {
     const dialogRef = this.dialog.open(UpdateProfileDialog, {
-      data: {
-        firstName: this.userData()?.firstName,
-        lastName: this.userData()?.lastName,
-        gender: this.userData()?.gender
-      },
+        data: {
+            firstName: this.userData()?.firstName,
+            lastName: this.userData()?.lastName,
+            gender: this.userData()?.gender
+        },
     });
 
     dialogRef.afterClosed().subscribe(result => {
-      console.log('The update dialog dialog was closed');
+        console.log('The update dialog dialog was closed');
     });
-  }
+}
 }
